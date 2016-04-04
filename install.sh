7695088985190cfbc46371eac5575f71d0b3e328
Uilian Ries <uilian.ries@datacom.ind.br>
2016-04-04 10:56:47 -0300

    Added Effective STL
    Signed-off-by: Uilian Ries <uilian.ries@datacom.ind.br>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fortunes="effective_modern_cpp effective_cpp the_pragmatic_programmer"
+fortunes="effective_modern_cpp effective_cpp effective_stl the_pragmatic_programmer"
 
 for it in $fortunes
 do
@@ -9,4 +9,4 @@ do
   sudo cp $it.dat /usr/share/games/fortunes
 done
 
-echo 'fortune the_pragmatic_programmer effective_modern_cpp effective_cpp' >> ~/.bashrc
+echo "fortune $fortunes" >> ~/.bashrc
